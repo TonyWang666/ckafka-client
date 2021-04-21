ead085ae93588ece55428048ba21c1b07738d653
TonyWang666 <twang@cn.roblox.com>
2021-04-21 17:46:58 +0800

    test-with-li

---
+++ Luobu.Ckafka/Luobu.Ckafka/CkafkaProducer.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Confluent.Kafka;
+using System.Threading.Tasks;
+
+namespace Luobu.Ckafka
+{
+    public class CkafkaProducer
+    {
+        private string _CkafkaAddress;
+        private string _TopicName;
+        private IProducer<Null, string> _Producer;
+
+        public CkafkaProducer()
+        {
+        }
+
+        public CkafkaProducer(string cKafkaAddress, string topicName)
+        {
+            _CkafkaAddress = cKafkaAddress ?? throw new ArgumentNullException(nameof(cKafkaAddress));
+            _TopicName = topicName ?? throw new ArgumentNullException(nameof(topicName));
+            BuildProducer();
+        }
+
+        private void BuildProducer()
+        {
+            var config = new ProducerConfig { BootstrapServers = _CkafkaAddress };
+            _Producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        public async Task<DeliveryResult<Null, string>> PublishMessageAsync(string message)
+        {
+            //ProduceException<Null, string> might be generated
+            return await _Producer.ProduceAsync(_TopicName, new Message<Null, string> { Value = message });
+        }
+    }
+}
